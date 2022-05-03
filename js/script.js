18bf6898151c40a1bed9bc5971fb3d89f8ba75d6
João Oneta <joaomatheusoneta01@gmail.com>
2022-05-03 20:47:02 -0300

    Implementada a funcionalidade de scroll na página através dos links do menu e botões da aplicação via JavaScript, assim tornando possivel acionar o efeito de scroll smooth em todos os navegadores atuais.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
+
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("header-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
